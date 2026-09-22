c0d8c80074884550d13d7cba6665033d2405f096
codeRangerProj <h4mres@gmail.com>
2026-09-22 16:41:00 +0300

    fix api

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [svgr({ exportAsDefault: true }), react()],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }],
   },
   define: {
-    __IS_DEV__: JSON.stringify(true),
-    __API__: JSON.stringify('http://localhost:8000'),
+    __IS_DEV__: JSON.stringify(mode !== 'production'),
+    __API__: JSON.stringify(
+      mode === 'production'
+        ? 'https://prod-proj-qa6f.onrender.com'
+        : 'http://localhost:8000',
+    ),
     __PROJECT__: JSON.stringify('frontend'),
   },
-});
+}));
